a10eef222bdbea07c96e45f5feb1396024ae3e06
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2024-10-24 16:28:12 +0530

    Update authenticate.js
    Adds ;

--- src/authenticate.js
+++ src/authenticate.js
@@ -1,19 +1,19 @@
 "use strict";
-const auth = require('basic-auth')
-const LOGIN = process.env.LOGIN
-const PASSWORD = process.env.PASSWORD
+const auth = require('basic-auth');
+const LOGIN = process.env.LOGIN;
+const PASSWORD = process.env.PASSWORD;
 
 function authenticate(req, res, next) {
   if (LOGIN && PASSWORD) {
     const credentials = auth(req)
     if (!credentials || credentials.name !== LOGIN || credentials.pass !== PASSWORD) {
-      res.setHeader('WWW-Authenticate', `Basic realm="Bandwidth-Hero Compression Service"`)
+      res.setHeader('WWW-Authenticate', `Basic realm="Bandwidth-Hero Compression Service"`);
 
-      return res.status(401).end('Access denied')
+      return res.status(401).end('Access denied');
     }
   }
 
-  next()
+  next();
 }
 
-module.exports = authenticate
+module.exports = authenticate;

f70af6c7bdd2ee1411996c46477cd45927ff7515
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2024-10-24 17:00:22 +0530

    Update params.js
    Enforce HTTPS

--- src/params.js
+++ src/params.js
@@ -1,21 +1,14 @@
 "use strict";
 const DEFAULT_QUALITY = 40
 
-function enforceHTTPS(req, res, next) {
-  let url = req.query.url;
-
-  // If the URL starts with 'http:', change it to 'https:'
-  if (url && url.startsWith('http:')) {
-    req.query.url = url.replace('http:', 'https:');
-  }
-
-  next();
-}
 
 function params(req, res, next) {
   let url = req.query.url;
   if (!url) return res.send('bandwidth-hero-proxy');
-
+  // If the URL starts with 'http:', change it to 'https:'
+  if (url && url.startsWith('http:')) {
+    req.query.url = url.replace('http:', 'https:');
+  }
   req.params.url = decodeURIComponent(url);
   req.params.webp = !req.query.jpeg
   req.params.grayscale = req.query.bw != 0
@@ -23,4 +16,4 @@ function params(req, res, next) {
 
   next()
 }
- module.exports = {enforceHTTPS,params};
+ module.exports = params;

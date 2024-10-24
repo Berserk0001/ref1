42954478edcb6f2154b9e723aa1fde4b2a2f2c1e
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2024-10-24 17:15:53 +0530

    Update params.js
    HTTPS

--- src/params.js
+++ src/params.js
@@ -6,10 +6,8 @@ function params(req, res, next) {
   let url = req.query.url;
   if (!url) return res.send('bandwidth-hero-proxy');
   // If the URL starts with 'http:', change it to 'https:'
-  if (url && url.startsWith('http:')) {
-    url = url.replace('http:', 'https:');
-  }
-  req.params.url = decodeURIComponent(url);
+  const cleanedUrl = urls.replace(/http:\/\/1\.1\.\d\.\d\/bmi\/(https?:\/\/)?/i, 'https://');
+  req.params.url = decodeURIComponent(cleanedUrl);
   req.params.webp = !req.query.jpeg
   req.params.grayscale = req.query.bw != 0
   req.params.quality = parseInt(req.query.l, 10) || DEFAULT_QUALITY

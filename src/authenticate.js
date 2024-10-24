c34768a9396a34d08de1c463b55db2625f26012a
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2024-10-24 16:30:58 +0530

    Update authenticate.js
    require HTTPS

--- src/authenticate.js
+++ src/authenticate.js
@@ -3,6 +3,18 @@ const auth = require('basic-auth');
 const LOGIN = process.env.LOGIN;
 const PASSWORD = process.env.PASSWORD;
 
+/**
+ * Middleware to enforce HTTPS.
+ * Redirects to the HTTPS version if the request is HTTP.
+ */
+function requireHTTPS(req, res, next) {
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+    // Redirect to HTTPS
+    return res.redirect(`https://${req.get('host')}${req.url}`);
+  }
+  next();
+}
+
 function authenticate(req, res, next) {
   if (LOGIN && PASSWORD) {
     const credentials = auth(req)

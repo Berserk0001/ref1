8274a1c9c8f7f543f7dea2a94f685b7665784cad
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2024-10-26 18:40:28 +0530

    Update proxy.js

--- src/proxy.js
+++ src/proxy.js
@@ -25,7 +25,7 @@ async function proxy(req, res) {
       {
       headers: {
         ...pick(req.headers, ["cookie", "dnt", "referer", "range"]),
-        "user-agent": "Bandwidth-Hero Compressor",
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/114.0",
         via: "1.1 bandwidth-hero",
       },
       maxRedirections: 4

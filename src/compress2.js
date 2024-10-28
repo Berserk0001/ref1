bfcb9a7e2c5113d242aebb649dd79a97e1d54fc9
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2024-10-28 12:47:02 +0530

    Create compress2.js

---
+++ src/compress2.js
@@ -0,0 +1,50 @@
+"use strict";
+/*
+ * compress.js
+ * A module that compresses an image
+ * compress(httpRequest, httpResponse, ReadableStream);
+ */
+const sharp = require('sharp');
+const redirect = require('./redirect');
+
+// Disable caching and set concurrency for sharp
+sharp.cache(true);
+sharp.concurrency(0);
+const sharpStream = () => sharp({ animated: false, unlimited: true });
+
+function compress(req, res, input) {
+  const format = 'jpeg';
+
+  input.data
+    .pipe(
+      sharpStream()
+        .grayscale(req.params.grayscale)
+        .toFormat(format, {
+          quality: req.params.quality,
+          chromaSubsampling: '4:2:0',
+        })
+    )
+    .toBuffer()
+    .then((output) => {
+      sharp(output)
+        .metadata()
+        .then((info) => {
+          res.setHeader('content-type', 'image/' + format);
+          res.setHeader('content-length', info.size);
+          res.setHeader('x-original-size', req.params.originSize);
+          res.setHeader('x-bytes-saved', req.params.originSize - info.size);
+          res.status(200).send(output);
+        })
+        .catch((err) => {
+          console.error("Metadata error:", err);
+          redirect(req, res);
+        });
+    })
+    .catch((err) => {
+      console.error("Compression error:", err);
+      redirect(req, res);
+    });
+}
+
+module.exports = compress;
+

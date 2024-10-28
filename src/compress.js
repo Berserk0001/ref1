6c28a2f098e287d364e6181202ecd96c96c08b20
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2024-10-28 23:25:48 +0530

    Update compress.js

--- src/compress.js
+++ src/compress.js
@@ -6,7 +6,7 @@
  */
 const sharp = require('sharp');
 const redirect = require('./redirect');
-sharp.cache(false); // Enables caching
+sharp.cache(true); // Enables caching
 sharp.concurrency(0); // Allows sharp to use all available CPU threads
 
 

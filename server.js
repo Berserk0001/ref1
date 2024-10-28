cf382d20b55063a574643462cd2f75ad563479ee
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2024-10-28 17:45:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 
 const app = require('express')();
 const params = require('./src/params');
-const proxy = require('./src/proxy2');
+const proxy = require('./src/proxy3');
 
 //app.enable('trust proxy');
 app.get('/', params, proxy);

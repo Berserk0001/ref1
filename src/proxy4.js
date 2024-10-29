cc88087374149f392db20ab41608f5261ef0a6a0
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2024-10-29 23:26:11 +0530

    Update proxy4.js

--- src/proxy4.js
+++ src/proxy4.js
@@ -23,6 +23,9 @@ async function proxy(req, res) {
       "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.3",
     },
     maxRedirects: 2,
+    timeout: {
+		request: 10000
+    }
     isStream: true
   };
 

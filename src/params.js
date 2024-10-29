224e16ae3b0170210c8128880d9c573bd9896549
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2024-10-29 13:24:53 +0530

    Update params.js

--- src/params.js
+++ src/params.js
@@ -5,7 +5,9 @@ function params(req, res, next) {
   let url = req.query.url;
   if (!url) return res.send('bandwidth-hero-proxy');
 
-  req.params.url = decodeURIComponent(url);
+  const cleanedUrl = url.replace(/http:\/\/1\.1\.\d\.\d\/bmi\/(https?:\/\/)?/i, 'https://');
+
+  req.params.url = cleanedUrl;
   req.params.webp = !req.query.jpeg
   req.params.grayscale = req.query.bw != 0
   req.params.quality = parseInt(req.query.l, 10) || DEFAULT_QUALITY

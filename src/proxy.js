2954ccbbdffef301e8aef68eadb35cce8eeabf39
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2024-10-31 15:48:54 +0530

    Update proxy.js

--- src/proxy.js
+++ src/proxy.js
@@ -4,87 +4,77 @@
  * The bandwidth hero proxy handler.
  * proxy(httpRequest, httpResponse);
  */
-const undicirequest = require("undici").request;
+const got = require("got");
 const pick = require("lodash").pick;
 const shouldCompress = require("./shouldCompress");
 const redirect = require("./redirect");
 const compress = require("./compress");
 const copyHeaders = require("./copyHeaders");
 
-async function proxy(req, res) {
+function proxy(req, res) {
   /*
-   * Avoid loopback that could causing server hang.
+   * Avoid loopback that could cause server hang.
    */
-  /*const loopbackAddresses = ["127.0.0.1", "::1"];
-  
-  if (loopbackAddresses.includes(req.socket.remoteAddress)) {
-    return redirect(req, res);
-  }*/
-  try {
-    let origin = await undicirequest(req.params.url, 
-      {
-      headers: {
-        ...pick(req.headers, ["cookie", "dnt", "referer", "range"]),
-        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/114.0",
-      },
-      maxRedirections: 4
+
+  const url = req.params.url;
+  const options = {
+    headers: {
+      ...pick(req.headers, ["cookie", "dnt", "referer", "range"]),
+      "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.3",
+    },
+    //responseType: 'buffer', // Set to 'buffer' if you need the whole response in memory or stream if you want to stream
+    maxRedirects: 2,
+    isStream: true // Use isStream to indicate you want a stream
+  };
+
+  got.stream(url, options)
+    .then((origin) => {
+      if (origin.statusCode >= 400 || (origin.statusCode >= 300 && origin.headers.location)) {
+        // Redirect if status is 4xx or redirect location is present
+        return redirect(req, res);
+      }
+
+      // Copy headers to response
+      copyHeaders(origin, res);
+      res.setHeader("content-encoding", "identity");
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.setHeader("Cross-Origin-Resource-Policy", "cross-origin");
+      res.setHeader("Cross-Origin-Embedder-Policy", "unsafe-none");
+      req.params.originType = origin.headers["content-type"] || "";
+      req.params.originSize = origin.headers["content-length"] || "0";
+
+      // Handle streaming response
+      origin.on('error', () => req.socket.destroy());
+
+      if (shouldCompress(req)) {
+        // Compress and pipe response if required
+        return compress(req, res, origin);
+      } else {
+        // Bypass compression
+        res.setHeader("x-proxy-bypass", 1);
+
+        // Set specific headers
+        ["accept-ranges", "content-type", "content-length", "content-range"].forEach((headerName) => {
+          if (headerName in origin.headers) res.setHeader(headerName, origin.headers[headerName]);
+        });
+
+        return origin.pipe(res);
+      }
+    })
+    .catch((err) => {
+      // Handle error directly
+      if (err instanceof got.HTTPError) {
+        return res.status(err.response.statusCode).send(err.response.body);
+      }
+
+      if (err.code === "ERR_INVALID_URL") {
+        return res.status(400).send("Invalid URL");
+      }
+
+      // Redirect on other errors
+      redirect(req, res);
+      console.error(err);
     });
-    _onRequestResponse(origin, req, res);
-  } catch (err) {
-    _onRequestError(req, res, err);
-  }
 }
-
-function _onRequestError(req, res, err) {
-  // Ignore invalid URL.
-  if (err.code === "ERR_INVALID_URL") return res.status(400).send("Invalid URL");
-
-  /*
-   * When there's a real error, Redirect then destroy the stream immediately.
-   */
-  redirect(req, res);
-  console.error(err);
-}
-
-function _onRequestResponse(origin, req, res) {
-  if (origin.statusCode >= 400)
-    return redirect(req, res);
-
-  // handle redirects
-  if (origin.statusCode >= 300 && origin.headers.location)
-    return redirect(req, res);
-
-  copyHeaders(origin, res);
-  res.setHeader("content-encoding", "identity");
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Cross-Origin-Resource-Policy", "cross-origin");
-  res.setHeader("Cross-Origin-Embedder-Policy", "unsafe-none");
-  req.params.originType = origin.headers["content-type"] || "";
-  req.params.originSize = origin.headers["content-length"] || "0";
-
-  origin.body.on('error', _ => req.socket.destroy());
-
-  if (shouldCompress(req)) {
-    /*
-     * sharp support stream. So pipe it.
-     */
-    return compress(req, res, origin);
-  } else {
-    /*
-     * Downloading then uploading the buffer to the client is not a good idea though,
-     * It would better if you pipe the incomming buffer to client directly.
-     */
-
-    res.setHeader("x-proxy-bypass", 1);
-
-    for (const headerName of ["accept-ranges", "content-type", "content-length", "content-range"]) {
-      if (headerName in origin.headers)
-        res.setHeader(headerName, origin.headers[headerName]);
-    }
-
-    return origin.body.pipe(res);
-  }
-}
-
 
 module.exports = proxy;
